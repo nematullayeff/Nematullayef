19dfe8cc93d1030a7aa654fa9fb9b6ebb02f8ba5
nematullayeff <nematullayef99@gmail.com>
2023-05-05 15:59:50 +0500

    Uber

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,15 @@ document.querySelector(".menubtn").addEventListener("click", () => {
   document.querySelector(".Xbtn").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("translate-x-full");
   });
+
+  // Die //
+
+  var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    mousewheel: true,
+  });
